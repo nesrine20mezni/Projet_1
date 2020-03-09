9595cff125f1922653dc2e6a4150ff24d832ae87
nesrine20mezni <45823218+nesrine20mezni@users.noreply.github.com>
2020-03-09 12:29:25 +0000

    Add files via upload

---
+++ SearchServlet.java
@@ -0,0 +1,54 @@
+package isetj.Servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SearchServlet
+ */
+@WebServlet("/SearchServlet")
+public class SearchServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SearchServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String search=request.getParameter("search");
+		String moteur=request.getParameter("moteur");
+		
+		if(moteur.equals("google"))
+		{
+			response.sendRedirect("http://www.google.com/search?q="+search);
+			
+		}
+		else{
+			response.sendRedirect("http://search.yahoo.com/search?p="+search);
+
+		}
+	} 
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	
+	}
+
+}
